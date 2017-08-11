4e2fd903b1f61f4e62127c7feead4dae6877cb84
Edward Tang <tkwedward@gmail.com>
2017-08-11 14:21:02 -0700

    cash

--- job_data/admin.py
+++
@@ -1,7 +0,0 @@
-from django.contrib import admin
-from .models import Job_detail, User_data_form, Freelance, Test
-# Register your models here.
-admin.site.register(Job_detail)
-admin.site.register(Freelance)
-admin.site.register(User_data_form)
-admin.site.register(Test)
